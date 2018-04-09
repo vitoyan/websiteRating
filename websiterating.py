4c2a7267240c2c9ed9b3336492473e152413f525
Wei Yan <WYan@sonicwall.com>
2018-04-09 17:47:26 +0800

    update arguments

--- websiterating.py
+++ websiterating.py
@@ -11,26 +11,26 @@ logger = util.get_log('website_rating')
 
 def main():
     parser = argparse.ArgumentParser(description='Websites Dynamic Rating')
-    parser.add_argument('-b', '--basepath', help='The bath path to store the data set', type=str, required=False, default=str(path(pathlib.Path.home(), 'dataset')))
-    parser.add_argument('-i', '--init', action='store_true', default=False, dest='boolean_switch', help='initail environment')
-    parser.add_argument('-u', '--pages', action='store_true', default=False, dest='boolean_switch', help='prepare pages by url')
-    parser.add_argument('-d', '--dataset', action='store_true', default=False, dest='boolean_switch', help='prepare dataset')
-    parser.add_argument('-a', '--all', action='store_true', default=False, dest='boolean_switch', help='inital, prepare pages, prepare dataset')
+    parser.add_argument('-b', '--basepath', help='The bath path to store the data set', type=str, required=False, default=str(path(pathlib.Path.home(), 'dataset')), dest ='basepath')
+    parser.add_argument('-i', '--init', action='store_true', default=False, dest='init', help='initail environment')
+    parser.add_argument('-u', '--pages', action='store_true', default=False, dest='pages', help='prepare pages by url')
+    parser.add_argument('-d', '--dataset', action='store_true', default=False, dest='dataset', help='prepare dataset')
+    parser.add_argument('-a', '--all', action='store_true', default=False, dest='all', help='inital, prepare pages, prepare dataset')
 
     args = parser.parse_args()
     
     
-    dm = data_manager.DataManager(base_path = args.b) 
+    dm = data_manager.DataManager(base_path = args.basepath) 
 
-    if args.i or args.a:
+    if args.init or args.all:
         logger.info("Initail Environment")             
         dm.init_environment()
 
-    if args.u or args.a:
+    if args.pages or args.all:
         logger.info("preparing websites pages by url")
         dm.prepare_websites_pages_by_url_files()
 
-    if args.d or args.a:
+    if args.dataset or args.all:
         logger.info("preparing data set")
         dm.prepare_train_data()
         dm.prepare_valid_data()
