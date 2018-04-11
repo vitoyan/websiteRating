d0c7ecee44247b80e2a78a01b48e6ba04a7f78be
Wei Yan <WYan@sonicwall.com>
2018-04-11 16:32:04 +0800

    add ALL argument

--- websiterating.py
+++ websiterating.py
@@ -14,9 +14,10 @@ def main():
     parser.add_argument('-i', '--init', action='store_true', default=False, dest='init', help='initail environment')
     parser.add_argument('-u', '--pages', action='store_true', default=False, dest='pages', help='prepare pages by url')
     parser.add_argument('-d', '--dataset', action='store_true', default=False, dest='dataset', help='prepare dataset')
-    parser.add_argument('-a', '--prepareall', action='store_true', default=False, dest='prepareall', help='inital, prepare pages, prepare dataset')
+    parser.add_argument('-P', '--prepareall', action='store_true', default=False, dest='prepareall', help='inital, prepare pages, prepare dataset')
     parser.add_argument('-t', '--train', help='The base path to store the data set', type=str, required=False, default='SGD', dest ='train')
     parser.add_argument('-T', '--trainall', action='store_true', default=False, dest='trainall', help='train all data with all methods')
+    parser.add_argument('-A', '--all', action='store_true', default=False, dest='all', help='do all')
 
 
     args = parser.parse_args()
@@ -24,15 +25,15 @@ def main():
     
     dm = data_manager.DataManager(base_path = args.basepath) 
 
-    if args.init or args.prepareall:
+    if args.init or args.prepareall or args.all:
         logger.info("Initail Environment")             
         dm.init_environment()
 
-    if args.pages or args.prepareall:
+    if args.pages or args.prepareall or args.all:
         logger.info("preparing websites pages by url")
         dm.prepare_websites_pages_by_url_files()
 
-    if args.dataset or args.prepareall:
+    if args.dataset or args.prepareall or args.all:
         logger.info("preparing data set")
         dm.prepare_train_data()
         dm.prepare_valid_data()
@@ -41,23 +42,23 @@ def main():
     wrm = website_rating_model.WebsiteRatingModel(dm.train_path, dm.valid_path)
     wrm.load_data()
 
-    if args.train.upper() == 'SGD' or args.trainall:
+    if args.train.upper() == 'SGD' or args.trainall or args.all:
         logger.info('training with SGD')
         wrm.SGDClassifier_train_model()  
 
-    if args.train.upper() == 'MNB' or args.trainall:
+    if args.train.upper() == 'MNB' or args.trainallor or args.all:
         logger.info('training with MNB')
         wrm.MultinomialNB_train_model()
 
-    if args.train.upper() == 'BNB' or args.trainall:
+    if args.train.upper() == 'BNB' or args.trainall or args.all:
         logger.info('training with BNB')
         wrm.BernoulliNB_train_model()
 
-    if args.train.upper() == 'SVC' or args.trainall:
+    if args.train.upper() == 'SVC' or args.trainall or args.all:
         logger.info('training with SVC')
         wrm.SVC_train_model()
 
-    if args.train.upper() == 'DTC' or args.trainall:
+    if args.train.upper() == 'DTC' or args.trainall or args.all:
         logger.info('training with dtc')
         wrm.DecisionTreeClassifier_train_model()  
 
